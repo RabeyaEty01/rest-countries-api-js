d4927a43d17b5b150a1ce0b5ea1a36cacd84431f
RabeyaEty01 <etyrabeya01@gmail.com>
2021-09-23 19:23:52 +0600

    all done

--- js/country.js
+++ js/country.js
@@ -5,16 +5,18 @@ const loadCountries = () => {
 }
 const displayCountries = countries => {
     console.log(countries[0]);
+    const container = document.getElementById('countries');
     const countryHTML = countries.map(country => getCountryHTML(country));
-    container.innerHTML=countryHTML.join(' ');
+    container.innerHTML = countryHTML.join(' ');
     console.log(countryHTML[0]);
 }
 const getCountryHTML = country => {
+    const { name, capital, flag } = country;
     return `
     <div class="country">
-      <h2>${country.name}</h2>
-      <h4>Capital:${country.capital}</h4>
-      <img src="${country.flag}">
+      <h2>${name}</h2>
+      <h4>Capital:${capital}</h4>
+      <img src="${flag}">
     </div>`
 }
 loadCountries();
